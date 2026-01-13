bf44c46e5d5a07b25ba35be560b4bb8936aa1005
f.asadli@asan.local <f.esedli@asan.gov.az>
2026-01-13 16:11:38 +0400

    yeet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://innalok.runasp.net'
-export const SIGNALR_BASE_URL = "innalok.runasp.net/WebhookHub"
+export const SIGNALR_BASE_URL = "http://innalok.runasp.net/WebhookHub"
