8c07d4df3f46b23af5a131fe7d7b445adaaeefe4
f.asadli@asan.local <f.esedli@asan.gov.az>
2026-01-22 17:56:37 +0400

    yeet

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const config = defineConfig({
   plugins: [
@@ -17,6 +18,11 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      manifest: false,
+    })
   ],
 })
 
